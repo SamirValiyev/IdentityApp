ad1f8a65049b50dffee0fd34a0d9d49c7800dc5f
SamirValiyev <vliyevsamir5@gmail.com>
2025-06-04 23:24:59 +0400

    project created

---
+++ IdentityApp/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Context;
+
+public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+}
